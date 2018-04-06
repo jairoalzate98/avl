3991d8c5d75f89a077df52c6c2863b52eedaa77a
jairoalzate98 <jairo.alzate@uptc.edu.co>
2018-04-06 16:54:28 -0500

    se termino la clase node agregando sus atributos y los metodos set y get

--- src/models/Node.java
+++ src/models/Node.java
@@ -2,4 +2,31 @@ package models;
 
 public class Node{
 
+	private int information;
+	private Node right;
+	private Node left;
+
+	public Node(int information){
+		this.information = information;
+	}
+	
+	public void setRight(Node node){
+			this.right = right;
+	}
+	
+	public void setLeft(Node node){
+			this.left = left;
+	}
+	
+	public int getInformation(){
+		return information;
+	}
+	
+	public Node getRight(){
+		return right;
+	}
+	
+	public Node getLeft(){
+		return left;
+	}
 }
